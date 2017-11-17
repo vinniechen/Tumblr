a1b53b6e46ad9114bdd66313fe19852ccf302b97
Vinnie Chen <vinnie.chen211@gmail.com>
2017-11-17 14:26:06 -0800

    Finished Tumblr app, captions viewable with tap

---
+++ Tumblr/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Tumblr
+//
+//  Created by Vinnie Chen on 11/17/17.
+//  Copyright © 2017 Vinnie Chen. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
